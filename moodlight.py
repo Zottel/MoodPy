dfa09bd2bfa496bfda3d2ea7a1f1a7a19aa6c10b
Julius Roob <julius@juliusroob.de>
2013-07-20 22:01:28 +0200

    USB HID magic numbers no longer entirely magic

--- moodlight.py
+++ moodlight.py
@@ -83,7 +83,25 @@ class Moodlight:
 
 	def _sendmsg(self, msg):
 		print(msg)
-		self.dev.ctrl_transfer(0x21,0x09,0,0,msg)
+
+		# USB HID magic numbers
+		# taken from http://www.lvr.com/hidpage.htm
+		LIBUSB_ENDPOINT_IN = 0x80
+		LIBUSB_ENDPOINT_OUT = 0x00
+		LIBUSB_REQUEST_TYPE_CLASS = (0x01 << 5)
+		LIBUSB_RECIPIENT_INTERFACE = 0x01
+
+		CONTROL_REQUEST_TYPE_IN = LIBUSB_ENDPOINT_IN + LIBUSB_REQUEST_TYPE_CLASS + LIBUSB_RECIPIENT_INTERFACE
+		CONTROL_REQUEST_TYPE_OUT = LIBUSB_ENDPOINT_OUT + LIBUSB_REQUEST_TYPE_CLASS + LIBUSB_RECIPIENT_INTERFACE
+
+		HID_GET_REPORT = 0x01
+		HID_SET_REPORT = 0x09
+
+		# Writing message as HID report to device
+		self.dev.ctrl_transfer(CONTROL_REQUEST_TYPE_OUT, HID_SET_REPORT, 0, 0, msg)
+		# Reading back HID report from device
+		print(self.dev.ctrl_transfer(CONTROL_REQUEST_TYPE_IN, HID_GET_REPORT, 0, 0, 128))
+
 
 	def execute(self, sequence, looping = False):
 		msg = [b for cmd in [x.bytes for x in sequence] for b in cmd]
